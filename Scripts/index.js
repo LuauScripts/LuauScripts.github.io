524874c0eef822c9c998f7dd65a542fc8f743d86
htmlstuffs <117377030+htmlstuffs@users.noreply.github.com>
2023-03-10 11:49:33 -0500

    Update index.js

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,4 +1,10 @@
-if (window.location.href === "https://luauscripts.github.io/Scripts/index.html") {
-  window.location.href = "https://luauscripts.github.io/Scripts/index.html";
-  window.location.replace("https://luauscripts.gituhb.io/Scripts/");
-}
+window.onload = function () {
+  if (window.location.href === "https://luauscripts.github.io/Scripts/index.html") {
+    noIndexScripts()
+  }
+  function noIndexScripts() {
+    location.replace("https://luauscripts.github.io/Scripts/")
+  }
+  else if (window.location.href === "https://luauscripts.github.io/Scripts/") {
+  }
+};
