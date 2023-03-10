3fd27a081d9d0b8e0e8c0e6c6eeeaaf5ee6b482a
htmlstuffs <117377030+htmlstuffs@users.noreply.github.com>
2023-03-10 11:35:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+if (window.location.href === "https://luauscripts.github.io/index.html") {
+  window.location.href = "https://luauscripts.github.io/index.html";
+  window.location.replace("https://luauscripts.gituhb.io/");
+}
