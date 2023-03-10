4b90ddafe6d4b224125ec3b7b3398e317a3fc921
htmlstuffs <117377030+htmlstuffs@users.noreply.github.com>
2023-03-10 11:35:54 -0500

    Update index.js

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,1 +1,4 @@
-
+if (window.location.href === "https://luauscripts.github.io/Scripts/index.html") {
+  window.location.href = "https://luauscripts.github.io/Scripts/index.html";
+  window.location.replace("https://luauscripts.gituhb.io/Scripts/");
+}
